3d456a89f368eb86040176ed919b6016f88095f7
Andrea Cianfanelli <cinfa78@gmail.com>
2018-04-29 19:31:03 +0200

    Camera Shake e Camera
    Camera Shake, prefab CameraApparatus
    Bottone per chiamare a mano gli eventi scriptable.

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+    float intensity = 1f;
+    bool isShaking;
+    Transform restTransform;
+    Vector3 restPosition;
+    Quaternion restRotation;
+    private void Start()
+    {
+        isShaking = false;
+        restTransform = transform;
+        restPosition = transform.localPosition;
+        restRotation = transform.localRotation;
+        print(restRotation.eulerAngles);
+    }
+    public void Shake()
+    {
+        if(!isShaking)
+            StartCoroutine(ShakeCoroutine());
+    }
+    public IEnumerator ShakeCoroutine()
+    {
+        isShaking = true;
+        Vector3 newAngle;
+        while (isShaking)
+        {
+            //transform.position = restPosition.position + Vector3.one * Random.Range(-intensity, intensity);
+            newAngle = Vector3.one* Random.Range(-intensity, intensity) + restRotation.eulerAngles;
+            print(newAngle);
+            transform.localRotation = Quaternion.Euler(newAngle);
+            yield return null;
+        }
+        isShaking = false;
+    }
+
+    public void StopShake()
+    {
+        isShaking = false;
+        StopAllCoroutines();
+        ResetTransform();
+    }
+
+    void ResetTransform()
+    {
+        transform.position = restPosition;
+        transform.rotation = restRotation;
+        transform.localScale = restTransform.localScale;
+    }
+}
---
+++ Assets/Scripts/EventCaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SEvent))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SEvent myScript = (SEvent)target;
+        if (GUILayout.Button("Raise Event"))
+        {
+            myScript.Raise();
+        }
+    }
+}
